b542fc67a86ddc1158791fa477e09c7e836f8b36
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2022-04-19 10:42:44 +1200

    Change test project to be of test project type.

---
+++ DataStructures.Tests/LinkedList/LinkedListTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DataStructures.Tests.LinkedList;
+
+public class LinkedListTests
+{
+    private readonly IList<int> _linkedList;
+
+    public LinkedListTests()
+    {
+        _linkedList = new List<int>();
+    }
+
+    [Fact]
+    public void NewList_ReturnsEmptyList()
+    {
+        Assert.Empty(_linkedList);
+    }
+}
