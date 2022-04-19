039f748562c9a4585b5c7c60d14ae6955e5c57e4
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2022-04-19 17:16:08 +1200

    Unit tests for Linked Lists (#1)
    * Implement List and Collection general tests
    
    * Add enumerable test for GetEnumerator and add newlines for file endings
    
    * Add comment to describe how to change which implementation to test

---
+++ DataStructures.Tests/GeneralTests/EnumerableTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using DataStructures.Tests.TestHelpers;
+using Xunit;
+
+namespace DataStructures.Tests.GeneralTests;
+
+public class EnumerableTests
+{
+    private readonly IEnumerable<int> _enumerable;
+
+    protected EnumerableTests(IEnumerable<int> enumerable)
+    {
+        _enumerable = enumerable;
+    }
+    
+    [Fact]
+    public void GetEnumerator_ReturnsEnumerator()
+    {
+        var enumerator = _enumerable.GetEnumerator();
+        
+        Assert.NotNull(enumerator);
+    }
+}
